3407fee65e2a2d574427aab98583a367fa60e8be
Vu Thi Ngoc <56816494+ngocvt-0484@users.noreply.github.com>
2024-07-09 08:47:48 +0700

    Đăng ký

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  PERMITTED_ATTRIBUTES = %i(name email password avatar).freeze
+
+  before_save :downcase_email!
+
   has_many :posts, dependent: :destroy
   has_many :active_relationships, class_name: Follow.name,
                                   foreign_key: :follower_id,
@@ -13,6 +17,25 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
 
+  has_secure_password
+
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true,
+    format: {with: Settings.user.valid_email_regex}
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost:
+    end
+  end
+
+  private
+  def downcase_email!
+    email.downcase!
+  end
 end
