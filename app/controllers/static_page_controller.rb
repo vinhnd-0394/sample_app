7f66dbc0cd2897c3800da57de90cebff5bc3c2e4
vinhnd-0394 <ngu.duy.vinh@sun-asterisk.com>
2024-08-06 12:49:28 +0700

    [Naitei SJP] follow/unfollow

--- app/controllers/static_page_controller.rb
+++
@@ -1,3 +0,0 @@
-class StaticPageController < ApplicationController
-  def index; end
-end
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,6 @@
 class Follow < ApplicationRecord
   belongs_to :follower, class_name: User.name
   belongs_to :followed, class_name: User.name
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
